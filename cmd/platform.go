5e9de7f4d9e2559204df6ff8a3b8e0dd7abf2e48
Anton Swartz <djnecrogami@gmail.com>
2026-04-07 03:37:53 +0000

    feat(cmd): implement platform command with real detection
    Output matches platform_info() shell function format.
    
    Part of #25

--- cmd/platform.go
+++ cmd/platform.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/sshpub/dotfiles-cli/pkg/profile"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +11,28 @@ var platformCmd = &cobra.Command{
 	Use:   "platform",
 	Short: "Show detected platform information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("not implemented")
+		info := profile.DetectPlatform()
+		fmt.Println("Platform Information:")
+		fmt.Printf("  OS:              %s\n", info.OS)
+		fmt.Printf("  Architecture:    %s\n", info.Arch)
+		if info.Distro != "" {
+			fmt.Printf("  Distribution:    %s %s\n", info.Distro, info.DistroVersion)
+		}
+		if info.WSL {
+			fmt.Println("  WSL:             yes")
+		}
+		if info.MacOSVersion != "" {
+			fmt.Printf("  macOS Version:   %s\n", info.MacOSVersion)
+		}
+		if info.PkgManager != "" {
+			fmt.Printf("  Package Manager: %s\n", info.PkgManager)
+		}
+		if info.HomebrewPrefix != "" {
+			fmt.Printf("  Homebrew Prefix: %s\n", info.HomebrewPrefix)
+		}
+		if info.Container {
+			fmt.Println("  Container:       yes")
+		}
 	},
 }
 
